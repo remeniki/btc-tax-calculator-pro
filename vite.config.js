a35da280233225291997a9c474d3d0739da37110
remeniki <remeniki@gmail.com>
2025-08-07 17:00:15 +0200

    Changed project name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/btc-tax-calculator-github-pages/',
+  base: '/btc-tax-calculator-pro/',
   plugins: [react()],
 });
